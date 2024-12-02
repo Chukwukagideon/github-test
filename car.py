277d66003c317b378b3a16e9d966d47a28d2af15
Chukwukagideon <Chukwukagideon5@gmail.com>
2024-12-02 02:36:03 +0100

    Create car.py

---
+++ car.py
@@ -0,0 +1,21 @@
+# car game
+print("Welcome to car game, type start to start the car")
+key = input(">>").lower()
+
+car_started = False
+car_stopped = True
+
+button = ["start", "stop", "help"]
+for keypad in button:
+    if key == "start":
+        if car_started is False:
+            print("car started")
+        else:
+            car_started = True
+            print("car already started")
+
+    if key == "stop":
+        if car_stopped:
+            print("car stopped")
+        else:
+            print("car already stopped")
